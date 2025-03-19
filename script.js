d99b2a05ec69698818eb48d7de05edc098d04661
Andrei-C-M <andrei.c.manea@gmail.com>
2025-03-19 14:52:29 +0100

    added css and js

---
+++ script.js
@@ -0,0 +1,27 @@
+//pseudocode
+
+//initialize the game
+
+let questionIndex = 0;
+let score = 0;
+
+//create an array to store the questions and answers
+let questions = [
+    {   question1: "Meow?",
+        answers: ["Meow", "Woof", "Moo", "Oink"],
+        correct: "Meow"
+    },
+    {   question2: "Woof?",
+        answers: ["Meow", "Woof", "Moo", "Oink"],
+        correct: "Woof"
+    },
+    {   question3: "Moo?",
+        answers: ["Meow", "Woof", "Moo", "Oink"],
+        correct: "Moo"
+    },
+    {   question4: "Oink?",
+        answers: ["Meow", "Woof", "Moo", "Oink"],
+        correct: "Oink"
+    }];
+
+//create a function to display the questions and answers
